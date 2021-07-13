d5bf72e4b9c906d107b0e7691a1002de5189b4d9
Cybernaut <77c828fa@opayq.com>
2021-07-13 06:27:56 -0500

    Added Map component
    + Displays list of all countries from JSON map data
    + Added associated types
    - Removed placeholder div

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import mapData from "data/countries.json";
 
-function Map() {
-  return (
-    <div >
-      A future map!
-    </div>
-  );
+import { Feature } from "types/map";
+
+interface Props {}
+interface State {
+  countries: string[] | [];
+}
+
+class Map extends Component<Props, State> {
+  constructor(props: Object) {
+    super(props);
+    this.state = { countries: [] };
+  }
+
+  componentDidMount() {
+    this.setState({
+      countries: mapData.features.map(
+        (country: Feature) => country.properties.ADMIN
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Total countries: {this.state.countries.length}
+        <ol>
+          {this.state.countries.map((country, index) => (
+            <li key={index}>
+              {index+1}. {country}
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default Map;
---
+++ src/types/map.d.ts
@@ -0,0 +1,11 @@
+export interface Feature {
+  properties: { ADMIN: string; ISO_A3: string };
+}
+
+// interface Data {
+//   features: {
+//     type: string;
+//     properties: { ADMIN: string; ISO_A3: string };
+//     geometry: { type: string; coordinates: number[][][] };
+//   };
+// }
