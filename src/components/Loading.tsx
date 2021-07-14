4a21846a04728dabfb273d4551a5fdef38ec4470
Cybernaut <77c828fa@opayq.com>
2021-07-14 04:31:34 -0500

    Added first pure components
    + Loading component
    + Title component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+function Loading() {
+  return <span className="p-3 text-center text-white bg-blue-600">Loading...</span>;
+}
+
+export default Loading;
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <div className="max-w-full p-6 text-4xl text-center text-white bg-blue-600"> Geomania! </div>;
+}
+
+export default Title;
