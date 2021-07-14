c17914235cefc8bbb0570f4d73798b1770042506
Cybernaut <77c828fa@opayq.com>
2021-07-14 06:33:07 -0500

    Update Map.tsx
    + Added rendering map with Leaflet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
+import { MapContainer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { loadData } from "model/DataParser";
+import { GeoJSON as GeoJSONType } from "types/map";
+import * as AboutApp from "utils/AppInfo";
 
 interface Props {}
 interface State {
-  countries: string[] | [];
+  countries: GeoJSONType | null;
 }
 
 class Map extends Component<Props, State> {
-  componentDidMount() {}
+  constructor(props: Object) {
+    super(props);
+    this.state = { countries: null };
+  }
+
+  async componentDidMount() {
+    const geoData = await loadData(AboutApp.isInDevelopment())
+      .then((data) => data)
+      .catch((reason) => null);
+
+    this.setState({ countries: geoData });
+  }
 
   render() {
-    return <div className="flex flex-col flex-grow w-full bg-gray-400">Map component</div>;
+    return (
+      <div
+        className="flex flex-col flex-grow w-full bg-gray-400"
+        data-testid="interactive-map"
+      >
+        <MapContainer style={{ height: "60vh" }} zoom={2} center={[20, 100]}>
+          <GeoJSON data={this.state.countries?.features}></GeoJSON>
+        </MapContainer>
+      </div>
+    );
   }
 }
 
