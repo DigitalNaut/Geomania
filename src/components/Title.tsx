8eed0a2fb148d2f8427e017d8d2baaeb0353b29f
Cybernaut <77c828fa@opayq.com>
2021-07-14 09:19:59 -0500

    Update Title.tsx
    + Code formatting
    + Minor changes
    + Data test ID

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Title() {
-  return <div className="max-w-full p-6 text-4xl text-center text-white bg-blue-600"> Geomania! </div>;
+  return (
+    <div
+      className="max-w-full p-6 text-4xl text-center text-white bg-blue-600"
+      data-testid="app-title"
+    >
+      Geomania
+    </div>
+  );
 }
 
 export default Title;
