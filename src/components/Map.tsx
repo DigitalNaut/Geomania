1dbf6a1009bd565ea90999e05fcc24038f896aed
Cybernaut <77c828fa@opayq.com>
2021-07-14 09:19:11 -0500

    Update Map.tsx
    Decorated with Loading, Error and Development pure components

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,27 +5,47 @@ import "leaflet/dist/leaflet.css";
 import { GeoJSON as GeoJSONType } from "types/map";
 import AboutApp from "utils/AppInfo";
 import { loadData } from "model/DataParser";
+import DevelopmentModeTag from "components/DevelopmentModeTag";
+import Loading from "components/Loading";
+import Error from "components/Error";
 
-interface Props {}
+interface Props {
+  styles?: React.CSSProperties;
+}
 interface State {
   countries: GeoJSONType | null;
+  loading: Boolean;
+  error: Boolean;
 }
 
 class Map extends Component<Props, State> {
   constructor(props: Object) {
     super(props);
-    this.state = { countries: null };
+    this.state = {
+      countries: null,
+      loading: true,
+      error: false,
+    };
   }
 
   async componentDidMount() {
     const geoData = await loadData(AboutApp.isInDevelopment())
       .then((data) => data)
-      .catch((reason) => null);
+      .catch((reason) => {
+        this.setState({ error: true });
+        return null;
+      });
 
-    this.setState({ countries: geoData });
+    this.setState({
+      countries: geoData,
+      loading: false,
+    });
   }
 
   render() {
+    if (this.state.loading) return <Loading />;
+    if (this.state.error) return <Error />;
+
     return (
       <div
         className="flex flex-col flex-grow w-full bg-gray-400"
@@ -36,6 +56,9 @@ class Map extends Component<Props, State> {
           zoom={2}
           center={[25, 0]}
         >
+          <DevelopmentModeTag
+            text={this.state.countries?.features.length.toString()}
+          />
           <GeoJSON data={this.state.countries?.features}></GeoJSON>
         </MapContainer>
       </div>
