f97de09f9df8c3035fc111b15cdb4e1b181a7499
Cybernaut <77c828fa@opayq.com>
2021-07-14 09:21:05 -0500

    Update Home.tsx
    + Minor visual changes
    + Extracted visual styles to View level
    + Decorated with "begin" Button

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,28 @@
 import Title from "components/Title";
 import Map from "components/Map";
 import { Component } from "react";
+import Button from "components/Button";
 
 export class Home extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <div className="flex flex-col w-full h-screen">
+      <div className="flex flex-col w-full h-screen bg-gray-900">
         <Title />
-        <Map />
+        <Map
+          styles={{
+            height: "100%",
+            position: "relative",
+          }}
+        />
+        <Button
+          text="Begin"
+          styles={{
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        />
       </div>
     );
   }
