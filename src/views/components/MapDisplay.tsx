f0ffca2db700648b61d9830ba2b2117bdfec02c5
John B <johnbernalfsd@gmail.com>
2021-12-22 06:17:54 -0600

    Fix: GeoJsonObject types
    - Added types to onEachFeature function
    - Adjusted map view height
    - Commented out example marker
    - Organized imports

--- src/views/components/MapDisplay.tsx
+++ src/views/components/MapDisplay.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { MapContainer, GeoJSON, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, GeoJSON, TileLayer } from 'react-leaflet';
+import { GeoJsonObject, Feature, Geometry } from 'geojson';
 // import { PathOptions } from 'leaflet';
-import { GeoJsonObject } from 'geojson';
 // import colors from 'tailwindcss/colors';
-import L from "leaflet";
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import sensors from 'src/core/data/sensors.json';
+import countries from 'src/data/country-dots.json';
 
 // import AboutApp from 'src/utils/AppInfo';
 // import { loadData } from 'src/models/DataParser';
@@ -74,30 +74,30 @@ import sensors from 'src/core/data/sensors.json';
 // * https://codesandbox.io/s/react-leaflet-v3-x-geojson-with-typescript-not-rendering-geojson-points-v28ly?file=/src/Map.tsx
 
 L.Marker.prototype.options.icon = L.icon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
 });
 
 const center = { lat: 59.433421, lng: 24.75224 };
 
 export default function Map() {
-  const onEachFeature = (feature: any, layer: L.Layer) => {
+  const onEachFeature = (feature: Feature<Geometry, { popupContent: string }>, layer: L.Layer) => {
     if (feature.properties) {
       const { popupContent } = feature.properties;
       layer.bindPopup(popupContent);
     }
   };
   return (
-    <MapContainer style={{ height: '80vh', width: '100vw' }} center={center} zoom={2}>
+    <MapContainer style={{ height: '100vh', width: '100vw' }} center={center} zoom={2}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[59.43046, 24.728563]}>
+      {/* <Marker position={[59.43046, 24.728563]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
-      <GeoJSON data={sensors as GeoJsonObject} onEachFeature={onEachFeature} />
+      </Marker> */}
+      <GeoJSON data={countries as GeoJsonObject} onEachFeature={onEachFeature} />
     </MapContainer>
   );
 }
