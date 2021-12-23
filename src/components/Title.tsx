f048fb57ea75c8d69280ae4bd831a519b87422e8
John B <johnbernalfsd@gmail.com>
2021-12-23 04:02:04 -0600

    Cleaned up font style

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title(): JSX.Element {
   return (
     <div
-      className="max-w-full p-6 text-4xl font-thin text-center text-white uppercase bg-green-700 font-playtone "
+      className="max-w-full p-6 text-4xl text-center text-white uppercase bg-green-700 font-playtone"
       data-testid="app-title"
     >
       Geomania
