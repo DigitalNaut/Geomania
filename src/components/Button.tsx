c6f95211a853b3fe4cbd0c8734277b18871f4847
John B <johnbernalfsd@gmail.com>
2021-12-23 03:20:14 -0600

    Changed component styles to green

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ children }: Props): JSX.Element {
   return (
     <button
       type="button"
-      className="w-1/3 p-4 text-xl font-medium text-center text-white bg-blue-700 rounded-md shadow-md cursor-pointer select-none hover:bg-blue-600 hover:shadow-lg"
+      className="w-full p-4 text-xl font-bold text-center text-white bg-green-700 rounded-md shadow-md cursor-pointer select-none hover:bg-green-600 hover:shadow-lg"
     >
       {children}
     </button>
