efc841985049d5aa265ecc6f1ffbbc3bc07fa53c
John B <johnbernalfsd@gmail.com>
2021-12-23 21:18:05 -0600

    Updated Tailwind theme colors
    Changed "trueGray" to "neutral" to remove Tailwind warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.trueGray,
+      gray: colors.neutral,
       red: colors.red,
       yellow: colors.amber,
       green: colors.lime,
