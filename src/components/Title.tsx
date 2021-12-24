9d4018751181e161df6b8829601728c57d18d56f
John B <johnbernalfsd@gmail.com>
2021-12-24 03:39:28 -0600

    Fixed title styles

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title(): JSX.Element {
   return (
     <div
-      className="max-w-full p-6 text-4xl text-center text-white uppercase bg-green-800 shadow-lg drop-shadow-sm font-playtone"
+      className="max-w-full p-2 text-3xl text-white uppercase bg-green-800 shadow-lg drop-shadow-sm font-playtone"
       data-testid="app-title"
     >
       Geomania
