2a7908e99a1904db600f4307bc1e88a8effcae88
John B <johnbernalfsd@gmail.com>
2021-12-26 04:24:58 -0600

    Completed FC types and props
    - Added defaultProps
    - Constricted types to that of a regular button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 interface Props {
   children: string;
+  fit?: true;
 }
 
-export default function Button({ children }: Props): JSX.Element {
+const Button: React.FC<
+  Props & React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+> = ({ children, fit }) => {
+  const className =
+    'p-4 text-xl font-bold text-center text-white bg-green-700 rounded-md shadow-md cursor-pointer select-none hover:bg-green-600 hover:shadow-lg';
+
   return (
-    <button
-      type="button"
-      className="w-full p-4 text-xl font-bold text-center text-white bg-green-700 rounded-md shadow-md cursor-pointer select-none hover:bg-green-600 hover:shadow-lg"
-    >
+    <button type="button" className={[className, fit ? 'w-min' : 'w-full'].join()}>
       {children}
     </button>
   );
-}
+};
+
+Button.defaultProps = {
+  fit: undefined,
+};
+
+export default Button;
