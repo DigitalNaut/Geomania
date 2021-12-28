0205a95b1bbd2734d43b8c1f490d750df07cd665
John B <johnbernalfsd@gmail.com>
2021-12-28 05:04:51 -0600

    Map loads GeoJSON
    - Added styles for countries
    - Added useState and useEffect for lazy loading of GeoJSON data

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import Leaflet from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import Leaflet, { PathOptions } from 'leaflet';
+import { GeoJsonObject } from 'geojson';
 
 import { MAPBOX_TILEMAP_URL, OSM_TILEMAP_URL } from 'src/resources';
+import colors from 'tailwindcss/colors';
 
 // * On Styling layers: https://leafletjs.com/examples/choropleth/
 // * On Markers: https://codesandbox.io/s/react-leaflet-v3-x-geojson-with-typescript-not-rendering-geojson-points-v28ly?file=/src/Map.tsx
@@ -14,9 +16,37 @@ const bottomRightCorner = Leaflet.latLng(90, 250);
 const maxBounds = Leaflet.latLngBounds(lopLeftCorner, bottomRightCorner);
 const isUseMapbox = false;
 
+const countryStyle: PathOptions = {
+  fillColor: colors.blue[600],
+  fillOpacity: 1,
+  color: colors.gray[300],
+  weight: 1,
+  interactive: false,
+};
+
+export async function loadCountryGeometry(): Promise<GeoJsonObject> {
+  const data = (await import('src/data/continent-africa.json')) as GeoJsonObject;
+  console.log('Geometry JSON:', data.type);
+
+  return new Promise((resolve, reject) => {
+    if (data) resolve(data);
+    else reject(new Error());
+  });
+}
+
 // Viewport
 
 const Map: React.FC = ({ children }) => {
+  const [countryGeometry, setCountryGeometry] = useState<GeoJsonObject>();
+
+  useEffect(() => {
+    async function getGeometry() {
+      const geometry = await loadCountryGeometry();
+      setCountryGeometry(geometry);
+    }
+    getGeometry();
+  }, []);
+
   return (
     <MapContainer
       center={maxBounds.getCenter()}
@@ -30,6 +60,7 @@ const Map: React.FC = ({ children }) => {
         attribution="&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
         url={isUseMapbox ? MAPBOX_TILEMAP_URL : OSM_TILEMAP_URL}
       />
+      {countryGeometry && <GeoJSON data={countryGeometry} style={countryStyle} />}
       {children}
     </MapContainer>
   );
