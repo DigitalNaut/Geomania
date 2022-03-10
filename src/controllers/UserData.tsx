36d874ca1132fb24d43c495d90acab5eb54b3060
John B <johnbernalfsd@gmail.com>
2022-03-10 10:47:24 -0600

    Cleaned up UserData controller
    - Renamed from "database"
    - Removed unnecessary code
    - Fixed styles
    - Formatting

--- src/controllers/UserData.tsx
+++ src/controllers/UserData.tsx
@@ -4,15 +4,16 @@ import * as Realm from 'realm-web';
 const REALM_APP_ID = 'geomania-gxxmr'; // e.g. myapp-abcde
 export const realmApp: Realm.App = new Realm.App({ id: REALM_APP_ID });
 
-// Create a component that displays the given user's details
+// Displays the given user's details
 export const UserDetail: React.FC<{ user: Realm.User }> = ({ user }) => {
   return (
-    <div>
-      <h1>Logged in with anonymous id: {user.id} </h1>
+    <div className="text-white">
+      <h1>Logged in with id: {user.id} </h1>
     </div>
   );
 };
-// Create a component that lets an anonymous user log in
+
+// Lets an anonymous user log in
 export const Login: React.FC<{ setUser: (user: Realm.User) => void }> = ({ setUser }) => {
   const loginAnonymous = async () => {
     const user: Realm.User = await realmApp.logIn(Realm.Credentials.anonymous());
@@ -25,24 +26,9 @@ export const Login: React.FC<{ setUser: (user: Realm.User) => void }> = ({ setUs
   );
 };
 
-// type CountryFeature = {
-//   _id: string;
-//   type: string;
-//   properties: {
-//     ADMIN: string;
-//     ISO_A3: string;
-//   };
-//   geometry: {
-//     type: string;
-//     coordinates: number[][][][];
-//   };
-// };
-
-export async function getData(user: Realm.User, countryAlpha3: string) {
+export async function getCountryGeometry(user: Realm.User, countryAlpha3: string) {
   const response: string = await user.functions.getCountryGeometry(countryAlpha3);
   const json: JSON = await JSON.parse(response);
-
-  console.log(json);
 
   return json;
 }
@@ -50,5 +36,5 @@ export async function getData(user: Realm.User, countryAlpha3: string) {
 export default {
   UserDetail,
   Login,
-  getData,
+  getCountryGeometry,
 };
