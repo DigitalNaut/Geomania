ef0da3e86de7146386087b16d32398508e09758f
John B <johnbernalfsd@gmail.com>
2023-07-10 07:05:10 -0600

    Improve ErrorFallback component

--- src/components/common/ErrorFallback.tsx
+++ src/components/common/ErrorFallback.tsx
@@ -1,3 +1,6 @@
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 type ErrorFallbackProps = {
   error: Error;
   resetErrorBoundary: () => void;
@@ -8,17 +11,20 @@ export default function ErrorFallback({
   resetErrorBoundary,
 }: ErrorFallbackProps) {
   return (
-    <div className="flex h-screen flex-1 items-center justify-center bg-slate-900 p-6">
+    <div className="flex h-screen flex-1 items-center justify-center bg-slate-900 p-6 text-white">
       <div
         role="alert"
-        className="flex h-fit w-full max-w-full flex-col flex-wrap gap-2 overflow-auto rounded-lg bg-red-800 p-4 shadow-md sm:w-2/3 md:w-1/2"
+        className="flex h-fit w-full max-w-full flex-col flex-wrap gap-2 overflow-auto rounded-lg bg-slate-800 p-4 shadow-md sm:w-2/3 md:w-1/2"
       >
         <h2 className="text-lg font-bold">Something went wrong:</h2>
-        <code className="w-full rounded-md bg-red-900 p-2">
-          {error.message}
-        </code>
+        <div className="flex w-full gap-2 rounded-md bg-slate-900 p-2">
+          <span>
+            <FontAwesomeIcon icon={faTriangleExclamation} />
+          </span>
+          <code>{error.message}</code>
+        </div>
         <button
-          className="rounded-md bg-red-400 p-2 hover:bg-red-300"
+          className="rounded-md bg-red-500 p-2 font-semibold hover:bg-red-400"
           role="button"
           onClick={resetErrorBoundary}
         >
