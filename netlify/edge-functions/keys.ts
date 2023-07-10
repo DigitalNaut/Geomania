09d51f893dad90705b58aa5554f538053873baaa
John B <johnbernalfsd@gmail.com>
2023-07-10 00:23:18 -0600

    Fix edge function data handling
    Use a parsed JSON object instead of a finicky array

--- netlify/edge-functions/keys.ts
+++ netlify/edge-functions/keys.ts
@@ -1,12 +1,12 @@
 import type { Context } from "https://edge.netlify.com/";
 
 export default (_: Request, { site }: Context) => {
-  const KEYS = [
-    Netlify.env.get("GOOGLE_API_KEY"),
-    Netlify.env.get("GOOGLE_CLIENT_ID"),
-  ];
+  const KEYS = {
+    apiKey: Netlify.env.get("GOOGLE_API_KEY"),
+    clientId: Netlify.env.get("GOOGLE_CLIENT_ID"),
+  };
 
   if (site.url === "http://geomaniac.netlify.app")
-    return new Response(KEYS.join(" "));
+    return new Response(JSON.stringify(KEYS));
   else return new Response("Not allowed", { status: 403 });
 };
