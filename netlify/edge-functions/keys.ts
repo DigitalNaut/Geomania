c9469a1fc985541e456eff8a232ad8412b986ea4
John B <johnbernalfsd@gmail.com>
2023-07-26 22:11:52 -0600

    Include more keys in edge function

--- netlify/edge-functions/keys.ts
+++ netlify/edge-functions/keys.ts
@@ -1,12 +1,12 @@
-import type { Context } from "https://edge.netlify.com/";
+import { type Context } from "https://edge.netlify.com/";
 
 export default (_: Request, { site }: Context) => {
+  if (site.url === "http://geomaniac.netlify.app") return new Response("Not allowed", { status: 403 });
+
   const KEYS = {
     apiKey: Netlify.env.get("GOOGLE_API_KEY"),
     clientId: Netlify.env.get("GOOGLE_CLIENT_ID"),
+    wikipediaApiUserAgent: Netlify.env.get("WIKIPEDIA_API_USER_AGENT"),
   };
-
-  if (site.url === "http://geomaniac.netlify.app")
-    return new Response(JSON.stringify(KEYS));
-  else return new Response("Not allowed", { status: 403 });
+  return new Response(JSON.stringify(KEYS));
 };
