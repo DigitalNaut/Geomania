6fe8d36a96439c0a52aa9a7160df2dd507f51677
John B <johnbernalfsd@gmail.com>
2023-07-27 03:15:15 -0600

    Fix Vite env variables

--- netlify/edge-functions/keys.ts
+++ netlify/edge-functions/keys.ts
@@ -1,12 +1,17 @@
 import { type Context } from "https://edge.netlify.com/";
 
-export default (_: Request, { site }: Context) => {
-  if (site.url === "http://geomaniac.netlify.app") return new Response("Not allowed", { status: 403 });
+export type EdgeKeys = {
+  apiKey?: string;
+  clientId?: string;
+};
 
-  const KEYS = {
+export default function getKeys(_: Request, { site }: Context) {
+  if (site.url !== Netlify.env.get("SITE_URL"))
+    return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 403 });
+
+  const KEYS: EdgeKeys = {
     apiKey: Netlify.env.get("GOOGLE_API_KEY"),
     clientId: Netlify.env.get("GOOGLE_CLIENT_ID"),
-    wikipediaApiUserAgent: Netlify.env.get("WIKIPEDIA_API_USER_AGENT"),
   };
-  return new Response(JSON.stringify(KEYS));
-};
+  return new Response(JSON.stringify(KEYS), { status: 200 });
+}
