6cac594168aca505c130fb27a37401f5c47e62cc
John B <johnbernalfsd@gmail.com>
2023-07-29 20:19:00 -0600

    Bug fix: Edge keys hook broken

--- src/hooks/useEdgeKeys.ts
+++ src/hooks/useEdgeKeys.ts
@@ -6,11 +6,11 @@ export function useEdgeKeys() {
   return useQuery({
     queryKey: ["keys"],
     queryFn: async () => {
-      const { text, status } = await fetch("/api/keys");
+      const response = await fetch("/api/keys");
 
-      if (status === 403) throw new Error("Unauthorized");
+      if (response.status === 403) throw new Error("Unauthorized");
 
-      const payload = await text();
+      const payload = await response.text();
 
       if (payload.length > 0) return JSON.parse(payload) as EdgeKeys;
       return undefined;
