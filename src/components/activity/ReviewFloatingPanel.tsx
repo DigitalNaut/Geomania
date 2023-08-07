ebac8f9b9184d95bd567d25edf00e32216c5cb7a
John B <johnbernalfsd@gmail.com>
2023-08-07 02:47:55 -0600

    Fixed bug where wiki details undefined would crash app

--- src/components/activity/ReviewFloatingPanel.tsx
+++ src/components/activity/ReviewFloatingPanel.tsx
@@ -118,14 +118,16 @@ export default function ReviewFloatingPanel({
       className="pointer-events-none absolute inset-x-0 bottom-8 z-[1000] mx-auto flex h-fit w-fit flex-col items-center gap-2 rounded-md"
       style={firstTrail}
     >
-      <details
-        className="pointer-events-auto rounded-md bg-sky-900/70 p-3 shadow-md backdrop-blur-md hover:bg-sky-900"
-        open={showDetails}
-        onToggle={(event) => setShowDetails(event.currentTarget.open)}
-      >
-        <summary className="cursor-pointer">Wikipedia summary</summary>
-        {shouldShow && showDetails && <CountryWikiInfo onError={onError} />}
-      </details>
+      {!disabled && (
+        <details
+          className="pointer-events-auto rounded-md bg-sky-900/70 p-3 shadow-md backdrop-blur-md hover:bg-sky-900"
+          open={showDetails}
+          onToggle={(event) => setShowDetails(event.currentTarget.open)}
+        >
+          <summary className="cursor-pointer">Wikipedia summary</summary>
+          {shouldShow && showDetails && <CountryWikiInfo onError={onError} />}
+        </details>
+      )}
       <div className="pointer-events-auto flex w-fit flex-col items-center overflow-hidden rounded-md bg-slate-900 drop-shadow-lg">
         <animated.div className="flex w-full flex-col items-center overflow-hidden rounded-md">
           <ActionButton disabled={disabled || !shouldShow} onClick={showNextCountry} title="Next country">
