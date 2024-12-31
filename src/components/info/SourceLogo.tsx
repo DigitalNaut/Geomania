61777a6dc87d775e0eafdb237d105b71578a9a5e
John Bernal <johnbernalfsd@gmail.com>
2024-12-31 01:39:00 -0600

    Refactor SourceLogo

--- src/components/info/SourceLogo.tsx
+++ src/components/info/SourceLogo.tsx
@@ -1,27 +1,15 @@
 import { useMemo } from "react";
 import { twMerge } from "tailwind-merge";
 
-const unsplashIconURL = "https://unsplash-assets.imgix.net/marketing/press-symbol.svg?auto=format&fit=crop&q=60";
-const wikiLogoURL =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/16px-Wikipedia-logo-v2.svg.png";
-
-const sourceLogos = {
-  unsplash: unsplashIconURL,
-  wikipedia: wikiLogoURL,
+const logoUrls = {
+  unsplash: "https://unsplash-assets.imgix.net/marketing/press-symbol.svg?auto=format&fit=crop&q=60",
+  wikipedia:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/16px-Wikipedia-logo-v2.svg.png",
 } as const;
 
-type Variant = keyof typeof sourceLogos;
+export default function SourceLogo({ className, source }: { className?: string; source: keyof typeof logoUrls }) {
+  const alt = useMemo(() => `${source.charAt(0).toUpperCase() + source.slice(1)} logo`, [source]);
+  const src = useMemo(() => logoUrls[source], [source]);
 
-export default function SourceLogo({ className, source }: { className?: string; source: Variant }) {
-  const sourceName = useMemo(() => `${source.charAt(0).toUpperCase() + source.slice(1)} logo`, [source]);
-
-  return (
-    <img
-      className={twMerge("w-4 inline-block", className)}
-      src={sourceLogos[source]}
-      alt={sourceName}
-      title={sourceName}
-      loading="lazy"
-    />
-  );
+  return <img className={twMerge("w-4 inline-block", className)} src={src} alt={alt} loading="lazy" />;
 }
