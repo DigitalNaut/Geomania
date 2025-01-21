d625cefed9315569186775090cb6d4d3eec7ffa1
John Bernal <johnbernalfsd@gmail.com>
2025-01-21 03:25:54 -0600

    Refactor the app router files main.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { faChartLine, faCog, faMap } from "@fortawesome/free-solid-svg-icons";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MotionConfig } from "motion/react";
+import type { PropsWithChildren } from "react";
 import { lazy, Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { Provider as ReduxProvider } from "react-redux";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 
-import ErrorFallback from "src/components/common/ErrorFallback";
 import { Spinner } from "src/components/common/Spinner";
 import Footer from "src/components/layout/Footer";
 import Nav from "src/components/layout/Header";
@@ -23,96 +23,77 @@ const LazyPageNotFound = lazy(() => import("src/pages/PageNotFound"));
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter(
-  [
-    {
-      element: (
-        <StandardLayout>
-          <Nav>
-            <Nav.Logo title="Geomaniac" />
+function Providers({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MotionConfig reducedMotion="user">
+        <ReduxProvider store={store}>
+          <HeaderControllerProvider>
+            <CountryFiltersProvider>
+              <CountryStoreProvider>
+                <Suspense fallback={<Spinner cover />}>{children}</Suspense>
+              </CountryStoreProvider>
+            </CountryFiltersProvider>
+          </HeaderControllerProvider>
+        </ReduxProvider>
+      </MotionConfig>
+    </QueryClientProvider>
+  );
+}
 
-            <div className="flex flex-1 gap-2 pl-6">
-              <Nav.Link to="/" icon={faMap}>
-                Map
-              </Nav.Link>
-              <Nav.Link to="../dashboard" icon={faChartLine}>
-                Dashboard
-              </Nav.Link>
-              <Nav.Link to="../settings" icon={faCog}>
-                Settings
-              </Nav.Link>
-            </div>
-          </Nav>
+function Layout() {
+  return (
+    <StandardLayout>
+      <Nav>
+        <div className="flex flex-1 gap-2 pl-6">
+          <Nav.Link to="/" icon={faMap}>
+            Map
+          </Nav.Link>
+          <Nav.Link to="../dashboard" icon={faChartLine}>
+            Dashboard
+          </Nav.Link>
+          <Nav.Link to="../settings" icon={faCog}>
+            Settings
+          </Nav.Link>
+        </div>
 
-          <Outlet />
+        <Nav.Logo title="Geomaniac" />
+      </Nav>
 
-          <Footer />
-        </StandardLayout>
-      ),
-      children: [
-        {
-          element: (
-            <ErrorBoundary FallbackComponent={ErrorFallback}>
-              <QueryClientProvider client={queryClient}>
-                <CountryFiltersProvider>
-                  <CountryStoreProvider>
-                    <Suspense fallback={<Spinner cover />}>
-                      <Outlet />
-                    </Suspense>
-                  </CountryStoreProvider>
-                </CountryFiltersProvider>
-              </QueryClientProvider>
-            </ErrorBoundary>
-          ),
-          children: [
-            {
-              path: "/",
-              index: true,
-              element: (
-                <MapContextProvider>
-                  <LazyActivityMap />
-                </MapContextProvider>
-              ),
-            },
-            {
-              path: "settings",
-              element: <LazySettings />,
-            },
-            {
-              path: "dashboard",
-              element: <LazyDashboard />,
-            },
-          ],
-        },
-        {
-          path: "*",
-          element: <LazyPageNotFound />,
-        },
-      ],
-    },
-  ],
-  {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_partialHydration: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_skipActionErrorRevalidation: true,
-    },
-  },
-);
+      <Outlet />
+
+      <Footer />
+    </StandardLayout>
+  );
+}
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            index
+            element={
+              <MapContextProvider>
+                <LazyActivityMap />
+              </MapContextProvider>
+            }
+          />
+          <Route path="/settings" element={<LazySettings />} />
+          <Route path="/dashboard" element={<LazyDashboard />} />
+          <Route path="/*" element={<LazyPageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default function App() {
   return (
-    <ReduxProvider store={store}>
-      <HeaderControllerProvider>
-        <RouterProvider
-          router={router}
-          future={{
-            v7_startTransition: true,
-          }}
-        />
-      </HeaderControllerProvider>{" "}
-    </ReduxProvider>
+    <Providers>
+      <Router />
+    </Providers>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MotionConfig } from "motion/react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
@@ -9,16 +7,10 @@ import ErrorFallback from "src/components/common/ErrorFallback";
 
 import "src/styles/output.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <QueryClientProvider client={queryClient}>
-        <MotionConfig reducedMotion="user">
-          <App />
-        </MotionConfig>
-      </QueryClientProvider>
+      <App />
     </ErrorBoundary>
   </StrictMode>,
 );
