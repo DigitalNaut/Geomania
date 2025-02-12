d35e02808db422abefce7663a6fdf407945d9f5b
John Bernal <johnbernalfsd@gmail.com>
2025-02-12 03:19:33 -0600

    Refactor feature manipulation commands into external script files

--- tools/src/svg-map/index.ts
+++ tools/src/svg-map/index.ts
@@ -1,4 +1,6 @@
 import chalk from "chalk";
+import fs from "fs";
+import path from "path";
 
 import { runCommand } from "./commandUtils.js";
 import { measureFileSize } from "./fileUtils.js";
@@ -22,89 +24,66 @@ if (!shapeFile) {
 
 console.log(chalk.green("Found shape file:"), shapeFile);
 
+const transposeFeatures = fs.readFileSync(path.join(__dirname, "scripts/transpose-features.js"), "utf-8");
+const transposeLabels = fs.readFileSync(path.join(__dirname, "scripts/transpose-labels.js"), "utf-8");
+
 /**
  * Command that:
  * - Explodes, simplifies, cleans, sorts, dissolves, projects and outputs the countries as GeoJSON
  * - Colors the countries and outputs them as SVG
  */
-const countriesCmd = `mapshaper "${shapeFile}"
-                      -clip bbox2=-180,-85,180,85
-                      -explode
+const countriesCmd = `\
+  mapshaper "${shapeFile}"
+    -clip bbox2=-180,-85,180,85
+    -explode
 
-                      -simplify weighted keep-shapes 10%
-                      -clean
-                      -proj EPSG:3857 target=*
+    -simplify weighted keep-shapes 10%
+    -proj EPSG:3857 target=*
 
-                      -target 1
-                      -each expression="
-                        if(this.isNull) return;
-                        if(!['Oceania', 'Europe'].includes(this.properties.CONTINENT)) return this.geojson = this.geojson;
+    -each expression="${transposeFeatures}"
 
-                        let maxX = -Infinity;
-                        this.geojson.geometry.coordinates = this.geojson.geometry.coordinates.map(
-                          (coords) => coords.map((coord) => {
-                            if (coord[0] < 40075017 * -0.25) {
-                              coord[0] += 40075017;
-                              maxX = Math.max(maxX, coord[0]);
-                            }
-                            return coord;
-                          }));
-                        
-                        if(maxX > -Infinity)
-                          this.properties.maxX = maxX;
+    -dissolve GEOUNIT copy-fields=SOVEREIGNT,SOV_A3,ADM0_DIF,ADMIN,ADM0_A3,GEOU_DIF,GEOUNIT,GU_A3,MAPCOLOR7,CONTINENT,SUBREGION,MIN_ZOOM,MIN_LABEL,MAX_LABEL,scalerank,LABEL_X,LABEL_Y,WIKIDATAID,ISO_A2_EH
 
-                        this.properties = this.properties;
-                        this.geojson = this.geojson;
-                      "
+    -each expression="${transposeLabels}"
 
-                      -dissolve GEOUNIT copy-fields=SOVEREIGNT,SOV_A3,ADM0_DIF,ADMIN,ADM0_A3,GEOU_DIF,GEOUNIT,GU_A3,MAPCOLOR7,CONTINENT,SUBREGION,MIN_ZOOM,MIN_LABEL,MAX_LABEL,scalerank,LABEL_X,LABEL_Y,WIKIDATAID,ISO_A2_EH
-                      -each expression="
-                        if(this.isNull) return;
-                        if(!['Oceania', 'Europe'].includes(this.properties.CONTINENT)) return this.properties = this.properties;
+    -clean
 
-                        if(this.properties.LABEL_X < -90)
-                        {
-                          this.properties.LABEL_X += 360;
-                        }
+    -sort 'GU_A3'
+    -o ${countryFeaturesFilename} format=json precision=0.01
 
-                        this.properties = this.properties;
-                      "
+    -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
+    -style fill="calcFill(MAPCOLOR7)" stroke="white" opacity=0.25
+    -o ${countriesMapFilename} id-field=GU_A3 format=svg
+  `.replace(/\s{2,}/g, " ");
 
-                      -sort 'GU_A3'
-                      -o ${countryFeaturesFilename} format=json precision=0.01
+const subregionsCmd = `\
+  mapshaper "${shapeFile}"
+    -clip bbox2=-180,-85,180,85
 
-                      -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
-                      -style fill="calcFill(MAPCOLOR7)" stroke="white" opacity=0.25
-                      -o ${countriesMapFilename} id-field=GU_A3 format=svg
-                      `.replace(/\s{2,}/g, " ");
+    -filter "GU_A3 != 'RUS' && GU_A3 != 'CAN' && GU_A3 != 'GRL'" + name="filtered_countries"
+    -target "filtered_countries"
+    -filter-islands min-vertices=25
+    -dissolve SUBREGION calc="LABEL_X = round(average(LABEL_X), 2), LABEL_Y = round(average(LABEL_Y), 2)" + name="subregions"
+    -rectangles target="subregions" + name="subregion_bounds"
+    -target "subregion_bounds"
+    -snap precision=0.01
+    -each "BBOX = this.bbox"
+    -o ${subregionFeaturesFilename} format=json precision=0.01
 
-const subregionsCmd = `mapshaper "${shapeFile}"
-                      -clip bbox2=-180,-85,180,85
+    -proj EPSG:3857
+    -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
+    -style fill="calcFill(this.id)" stroke="white" opacity=0.25
+    -o ${subregionBoundsFilename} format=svg id-field=SUBREGION
 
-                      -filter "GU_A3 != 'RUS' && GU_A3 != 'CAN' && GU_A3 != 'GRL'" + name="filtered_countries"
-                      -target "filtered_countries"
-                      -filter-islands min-vertices=25
-                      -dissolve SUBREGION calc="LABEL_X = round(average(LABEL_X), 2), LABEL_Y = round(average(LABEL_Y), 2)" + name="subregions"
-                      -rectangles target="subregions" + name="subregion_bounds"
-                      -target "subregion_bounds"
-                      -snap precision=0.01
-                      -each "BBOX = this.bbox"
-                      -o ${subregionFeaturesFilename} format=json precision=0.01
-
-                      -proj EPSG:3857
-                      -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
-                      -style fill="calcFill(this.id)" stroke="white" opacity=0.25
-                      -o ${subregionBoundsFilename} format=svg id-field=SUBREGION
-
-                      -dissolve SUBREGION target=1 + name="subregions"
-                      -target "subregions"
-                      -simplify weighted keep-shapes 10%
-                      -clean
-                      -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
-                      -style fill="calcFill(this.id)" stroke="white" opacity=0.25
-                      -proj EPSG:3857
-                      -o ${subregionsMapFilename} id-field=SUBREGION format=svg
-                      `.replace(/\s{2,}/g, " ");
+    -dissolve SUBREGION target=1 + name="subregions"
+    -target "subregions"
+    -simplify weighted keep-shapes 10%
+    -clean
+    -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
+    -style fill="calcFill(this.id)" stroke="white" opacity=0.25
+    -proj EPSG:3857
+    -o ${subregionsMapFilename} id-field=SUBREGION format=svg
+  `.replace(/\s{2,}/g, " ");
 
 /**
  * Command that:
@@ -113,32 +92,33 @@ const subregionsCmd = `mapshaper "${shapeFile}"
  * - Generates the continent boundaries and outputs both as SVG and GeoJSON data
  * - Simplifies the continents map and outputs the simplified map as SVG
  */
-const continentsCmd = `mapshaper "${shapeFile}"
-                      -clip bbox2=-180,-85,180,85
-                      -filter-islands min-vertices=50
+const continentsCmd = `\
+  mapshaper "${shapeFile}"
+    -clip bbox2=-180,-85,180,85
+    -filter-islands min-vertices=50
 
-                      -filter "GU_A3 != 'RUS' && GU_A3 != 'CAN' && GU_A3 != 'GRL'" + name="filtered_countries"
-                      -dissolve CONTINENT target="filtered_countries" calc="LABEL_X = round(average(LABEL_X), 2), LABEL_Y = round(average(LABEL_Y), 2)" + name="pruned_continents"
-                      -rectangles target="pruned_continents" + name="continent_bounds"
-                      -target "continent_bounds"
-                      -snap precision=0.01
-                      -each "BBOX = this.bbox"
-                      -o ${continentFeaturesFilename} format=json
+    -filter "GU_A3 != 'RUS' && GU_A3 != 'CAN' && GU_A3 != 'GRL'" + name="filtered_countries"
+    -dissolve CONTINENT target="filtered_countries" calc="LABEL_X = round(average(LABEL_X), 2), LABEL_Y = round(average(LABEL_Y), 2)" + name="pruned_continents"
+    -rectangles target="pruned_continents" + name="continent_bounds"
+    -target "continent_bounds"
+    -snap precision=0.01
+    -each "BBOX = this.bbox"
+    -o ${continentFeaturesFilename} format=json
 
-                      -proj EPSG:3857
-                      -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
-                      -style fill="calcFill(this.id)" stroke="white" opacity=0.25
-                      -o ${continentBoundsFilename} format=svg id-field=CONTINENT
+    -proj EPSG:3857
+    -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
+    -style fill="calcFill(this.id)" stroke="white" opacity=0.25
+    -o ${continentBoundsFilename} format=svg id-field=CONTINENT
 
-                      -dissolve CONTINENT target=1 + name="continents"
-                      -target "continents"
-                      -simplify weighted 3%
-                      -clean
-                      -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
-                      -style fill="calcFill(this.id)" stroke="white" opacity=0.25
-                      -proj EPSG:3857
-                      -o ${continentsMapFilename} id-field=CONTINENT format=svg
-                      `.replace(/\s{2,}/g, " ");
+    -dissolve CONTINENT target=1 + name="continents"
+    -target "continents"
+    -simplify weighted 3%
+    -clean
+    -colorizer random name=calcFill colors="#1f77b4,#ff7f0e,#2ca02c,#d62728,#9467bd,#8c564b,#e377c2" categories=0,1,2,3,4,5,6
+    -style fill="calcFill(this.id)" stroke="white" opacity=0.25
+    -proj EPSG:3857
+    -o ${continentsMapFilename} id-field=CONTINENT format=svg
+  `.replace(/\s{2,}/g, " ");
 
 async function processMaps() {
   let outputFiles: ReturnType<typeof measureFileSize>[] = [];
