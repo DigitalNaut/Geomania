79596a99ef18f4648335a33598e22243727b2f8b
John Bernal <johnbernalfsd@gmail.com>
2025-02-27 01:16:46 -0600

    Refactor of CountrySvgMap into pure components

--- src/components/map/CountrySvgMap.tsx
+++ src/components/map/CountrySvgMap.tsx
@@ -1,5 +1,5 @@
 import type { LeafletMouseEventHandlerFn } from "leaflet";
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, SVGAttributes } from "react";
 import { Fragment, useCallback, useMemo } from "react";
 
 import SvgMap from "src/components/common/SvgMap";
@@ -45,7 +45,7 @@ type SvgMapPaths = {
     : never]: SVGPathElement[];
 };
 
-function WithAnimationDefs({ children }: PropsWithChildren) {
+function WithWaveAnimationDefs({ children }: PropsWithChildren) {
   return (
     <>
       <defs>
@@ -96,30 +96,19 @@ function WithAnimationDefs({ children }: PropsWithChildren) {
   );
 }
 
-/**
- * SVG map implementation for countries
- * @param param0
- * @returns
- */
-export function CountrySvgMap({
-  hidden,
-  colorTheme,
-  onClick,
-  className,
-  lists: { activeList, highlightList, visitedList },
-}: {
-  hidden?: boolean;
-  colorTheme: SvgMapColorTheme;
-  onClick?: (a3: string) => void;
-  className?: string;
-  lists: ActiveSvgMapLists;
-}) {
-  const { zoom } = useMapContext();
-  const { paths, width, height, viewBox } = useSvgAttributes(mapSvg, ["width", "height", "viewBox"]);
+function useCountrySvgMap(
+  { activeList, highlightList, visitedList }: ActiveSvgMapLists,
+  {
+    onClick,
+  }: {
+    onClick?: (a3: string) => void;
+  },
+) {
+  const attributes = useSvgAttributes(mapSvg, ["width", "height", "viewBox"]);
 
-  const { visitedPaths, activePaths, highlightPaths, inactivePaths } = useMemo(
+  const paths = useMemo(
     () =>
-      paths.reduce<SvgMapPaths>(
+      attributes.paths.reduce<SvgMapPaths>(
         (acc, path) => {
           // Check if the country is active
           if (activeList.includes(path.id)) {
@@ -149,11 +138,8 @@ export function CountrySvgMap({
           inactivePaths: [],
         },
       ),
-    [activeList, highlightList, visitedList, paths],
+    [attributes.paths, activeList, highlightList, visitedList],
   );
-
-  const adjustForZoom = useCallback((value: number) => value / zoom ** 2, [zoom]);
-  const strokeWidth = adjustForZoom(3);
 
   const click: LeafletMouseEventHandlerFn = ({ originalEvent }) => {
     const { target } = originalEvent;
@@ -164,81 +150,118 @@ export function CountrySvgMap({
     if (a3) onClick?.(a3);
   };
 
+  return { paths, attributes, eventHandlers: { click } };
+}
+
+function CountryPath({
+  path,
+  ...rest
+}: {
+  path: SVGPathElement;
+} & Omit<SVGAttributes<SVGPathElement>, "path">) {
+  const d = useMemo(() => path.getAttribute("d"), [path]);
+
+  if (!d) return null;
+
+  return <path data-a3={path.id} d={d} {...rest} />;
+}
+
+function useZoomAdjustedLineStroke() {
+  const { zoom } = useMapContext();
+
+  /**
+   * Adjusts a value for the stroke width based on the map's zoom level.
+   *
+   * The value is divided by the square of the zoom level to narrow the stroke width as the map zooms in.
+   * @param zoom
+   * @returns
+   */
+  const scaleByZoom = useCallback((value: number) => value / zoom ** 2, [zoom]);
+
+  return { scaleByZoom };
+}
+
+/**
+ * SVG map implementation for countries
+ * @param param0
+ * @returns
+ */
+export function CountrySvgMap({
+  hidden,
+  colorTheme,
+  onClick,
+  className,
+  lists,
+}: {
+  hidden?: boolean;
+  colorTheme: SvgMapColorTheme;
+  onClick?: (a3: string) => void;
+  className?: string;
+  lists: ActiveSvgMapLists;
+}) {
+  const {
+    attributes: { width, height, viewBox },
+    paths: { activePaths, highlightPaths, visitedPaths, inactivePaths },
+    eventHandlers,
+  } = useCountrySvgMap(lists, { onClick });
+
+  const { scaleByZoom } = useZoomAdjustedLineStroke();
+
+  const style = useMemo(() => {
+    return { strokeWidth: scaleByZoom(3) };
+  }, [scaleByZoom]);
+
+  // Handle line drawing
+
   if (hidden) return null;
 
   return (
     <SvgMap
       className={className}
       svg={mapSvg}
-      eventHandlers={{ click }}
+      eventHandlers={eventHandlers}
       attributes={{ width, height, viewBox }}
       boundsAdjustment={manualBoundsAdjustment}
     >
       {inactivePaths.map((path, index) => (
-        <path
-          key={index}
-          data-a3={path.id}
-          d={path.getAttribute("d") ?? ""}
-          className={colorTheme.country.inactiveStyle}
-          style={{ strokeWidth }}
-        />
+        <CountryPath key={index} path={path} className={colorTheme.country.inactiveStyle} style={style} />
       ))}
 
       {visitedPaths.map((path, index) => (
-        <path
-          key={index}
-          data-a3={path.id}
-          d={path.getAttribute("d") ?? ""}
-          className={colorTheme.country.visitedStyle}
-          style={{ strokeWidth }}
-        />
+        <CountryPath key={index} path={path} className={colorTheme.country.visitedStyle} style={style} />
       ))}
 
       {activePaths.map((path, index) => (
-        <path
-          key={index}
-          data-a3={path.id}
-          d={path.getAttribute("d") ?? ""}
-          className={colorTheme.country.activeStyle}
-          style={{ strokeWidth }}
-        />
+        <CountryPath key={index} path={path} className={colorTheme.country.activeStyle} style={style} />
       ))}
 
       {highlightPaths.length > 0 && (
-        <WithAnimationDefs>
-          {highlightPaths.map((path) => {
-            const { id } = path;
-            const d = path.getAttribute("d");
+        <WithWaveAnimationDefs>
+          {highlightPaths.map((path) => (
+            <Fragment key={path.id}>
+              <CountryPath
+                path={path}
+                className={colorTheme.country.highlightStyle}
+                style={{ strokeWidth: scaleByZoom(2) }}
+              />
 
-            if (!d) return null;
+              <CountryPath
+                path={path}
+                pointerEvents="none"
+                className="fill-[url(#land)] stroke-none hover:fill-lime-600/30"
+              />
 
-            return (
-              <Fragment key={id}>
-                <path
-                  data-a3={id}
-                  d={d}
-                  className={colorTheme.country.highlightStyle}
-                  style={{ strokeWidth: adjustForZoom(2) }}
-                />
-                <path
-                  d={d}
-                  data-a3={id}
-                  pointerEvents="none"
-                  className="fill-[url(#land)] stroke-none hover:fill-lime-600/30"
-                />
-                <path
-                  d={d}
-                  data-a3={id}
-                  pointerEvents="none"
-                  className="animate-scrollDash fill-lime-500 [stroke-dasharray:32] [stroke-dashoffset:8] hover:[animation:none] hover:fill-[url(#waves)] hover:stroke-white hover:[stroke-dasharray:0]"
-                  style={{
-                    strokeWidth: adjustForZoom(4),
-                  }}
-                />
-              </Fragment>
-            );
-          })}
-        </WithAnimationDefs>
+              <CountryPath
+                path={path}
+                pointerEvents="none"
+                className="animate-scrollDash fill-lime-500 [stroke-dasharray:32] [stroke-dashoffset:8] hover:animate-none hover:fill-[url(#waves)] hover:stroke-white hover:[stroke-dasharray:0]"
+                style={{
+                  strokeWidth: scaleByZoom(4),
+                }}
+              />
+            </Fragment>
+          ))}
+        </WithWaveAnimationDefs>
       )}
     </SvgMap>
   );
